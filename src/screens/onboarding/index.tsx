344b4e566c7e196303b3e3c4435ee5a563b70415
FutureCake <thomas.c.hoek@gmail.com>
2025-11-14 16:24:55 +0100

    added all relevant onboarding navigator screens

--- src/screens/onboarding/index.tsx
+++ src/screens/onboarding/index.tsx
@@ -1,4 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Authentication from "./features/authentication";
+import Email from "./features/email";
 import Hello from "./features/hello";
 import Otp from "./features/otp";
 import Welcome from "./features/welcome";
@@ -9,8 +11,10 @@ const Stack = createNativeStackNavigator<OnboardingStackParamList>();
 export default function Onboarding() {
 
     return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Email">
             <Stack.Screen name="Hello" component={Hello} />
+            <Stack.Screen name="Authentication" component={Authentication} />
+            <Stack.Screen name="Email" component={Email} />
             <Stack.Screen name="Otp" component={Otp} />
             <Stack.Screen name="Welcome" component={Welcome} />
         </Stack.Navigator>
