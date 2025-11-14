3c4458bc1606946868958b9f519b11e55a2eedc4
FutureCake <thomas.c.hoek@gmail.com>
2025-11-14 14:04:29 +0100

    basic onboarding content

---
+++ src/screens/onboarding/features/otp/index.tsx
@@ -0,0 +1,4 @@
+
+export default function Otp() {
+    return null;
+}
--- src/screens/onboarding/index.tsx
+++ src/screens/onboarding/index.tsx
@@ -1,4 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Hello from "./features/hello";
+import Otp from "./features/otp";
+import Welcome from "./features/welcome";
 import { OnboardingStackParamList } from "./helpers/types";
 
 const Stack = createNativeStackNavigator<OnboardingStackParamList>();
@@ -7,7 +10,9 @@ export default function Onboarding() {
 
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Welcome" component={WelcomeScreen} />
+            <Stack.Screen name="Hello" component={Hello} />
+            <Stack.Screen name="Otp" component={Otp} />
+            <Stack.Screen name="Welcome" component={Welcome} />
         </Stack.Navigator>
     );
 }
